a8f04d06892d37f14214225f635b386e46f1f973
Joaopaulo55 <joaofranciscovicapaulo55@gmail.com>
2025-07-30 09:42:27 +0100

    *

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://bdownzyb.onrender.com/api';
+const API_BASE_URL = 'https://bdownzyb.onrender.com';
 
 document.addEventListener('DOMContentLoaded', function() {
     // DOM Elements
